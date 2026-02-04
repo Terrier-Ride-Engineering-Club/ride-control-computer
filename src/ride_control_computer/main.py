54135a13aa6118b577b77db4e687375e2a1acda1
jackjustus <jackjust@bu.edu>
2026-02-04 13:32:23 -0500

    Organize inputs in main

--- src/ride_control_computer/main.py
+++ src/ride_control_computer/main.py
@@ -7,8 +7,10 @@ from datetime import datetime
 from pathlib import Path
 
 from ride_control_computer.RCC import RCC
-from ride_control_computer.motor_controller.MockMotorController import MockMotorController
 from ride_control_computer.control_panel.MockControlPanel import MockControlPanel
+from ride_control_computer.motor_controller.MockMotorController import MockMotorController
+from ride_control_computer.motor_controller.RoboClawSerialMC import RoboClawSerialMotorController
+from ride_control_computer.motor_controller.RoboClaw import RoboClaw
 from ride_control_computer.theming_controller.MockThemeingController import MockThemingController
 from ride_control_computer.webserver.MockWebserverController import MockWebserverController
 
