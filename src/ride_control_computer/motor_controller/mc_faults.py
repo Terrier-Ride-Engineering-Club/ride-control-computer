7d15f6be7b0acfa750a9fadaa4871ec2c191fc6f
lmertens-bu <lmertens@bu.edu>
2026-03-25 17:59:06 -0400

    Revert "Added code for limit switch problem"
    This reverts commit 6dc231c77a6fdee20280de5d07414cdbb652e79e.

--- src/ride_control_computer/motor_controller/mc_faults.py
+++ src/ride_control_computer/motor_controller/mc_faults.py
@@ -29,8 +29,6 @@ def registerMotorControllerFaults(
     monitor: FaultMonitor,
     mc: MotorController,
     isMotionForbidden: Callable[[], bool],
-    isAtTopLimit: Callable[[], bool],
-    isAtBottomLimit: Callable[[], bool],
 ) -> None:
     """Register all motor-controller-related fault conditions.
 
@@ -62,10 +60,4 @@ def registerMotorControllerFaults(
         severity=FaultSeverity.MEDIUM,
         description="Motor motion detected while system is idle",
         condition=lambda: isMotionForbidden() and any(abs(s) > 10 for s in mc.getMotorSpeeds()),
-    ))
-    monitor.register(Fault(
-        code="MC_LIMIT_SWITCH_VIOLATION",
-        severity=FaultSeverity.HIGH,
-        description= lambda: f"Limit switches both active on tower: ", #im not sure what needs to go here to get the motor number
-        condition=lambda: isAtTopLimit() and isAtBottomLimit()
     ))
