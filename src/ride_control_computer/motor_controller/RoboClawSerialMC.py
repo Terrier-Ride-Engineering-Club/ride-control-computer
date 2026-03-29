54e1d5c2c5a0fd8cd6d50c8ef659ba23f9f3a981
jackjustus <jackjust@bu.edu>
2026-03-29 10:55:33 -0400

    Update RoboClawSerialMC.py

--- src/ride_control_computer/motor_controller/RoboClawSerialMC.py
+++ src/ride_control_computer/motor_controller/RoboClawSerialMC.py
@@ -57,7 +57,7 @@ class RoboClawSerialMotorController(MotorController):
     HEARTBEAT_TTL      = 0.025  # seconds — must stay above RoboClaw's 20ms timeout
 
     # --- Motion parameters ---
-    JOG_SPEED          = 800
+    JOG_SPEED          = 1100
     JOG_ACCELERATION   = 2000
     STOP_DECELERATION  = 2000
     HALT_DECELERATION  = 10000
